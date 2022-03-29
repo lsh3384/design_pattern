e8dc3588ba55925fae49c34426f3caa21b8875fc
lee <lsh3384@gmail.com>
2022-03-29 15:19:58 +0900

    Factory Method Pattern

---
+++ src/factory_method_pattern/Circle.java
@@ -0,0 +1,8 @@
+package factory_method_pattern;
+
+public class Circle implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Circle - draw() Method.");
+	}
+}
---
+++ src/factory_method_pattern/Rectangle.java
@@ -0,0 +1,8 @@
+package factory_method_pattern;
+
+public class Rectangle implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Rectangle - draw() Method");
+	}
+}
